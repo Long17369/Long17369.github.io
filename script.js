06840414cf9320408f220667d870c54e4e8bdfe8
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-26 07:40:15 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   word/7.26.json

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function loadWord(){
+function loadWord() {
 	var request = new XMLHttpRequest();
-	request.open('GET','./word/7.26.json');
-	request.responseType='json';
+	request.open('GET', './word/7.26.json');
+	request.responseType = 'json';
 	request.send();
-	request.onload = function(){
+	request.onload = function () {
 		var word = request.response;
 		showWord(word);
 	};
-}
-function showWord(word){
+};
+function showWord(word) {
 	var Word = document.getElementById('word');
 	var Creat;
 	var words = word.word;
-	Word.innerHTML=''
-	for(var i in words){
-		Creat = document.createElement('word'+i);
-		Creat.append(words[i]+'\n');
+	Word.innerHTML = ''
+	for (var i in words) {
+		Creat = document.createElement('word' + i);
+		Creat.append(words[i] + '\n');
 		var Creat1 = document.createElement('div');
 		Creat1.append(Creat);
 		Word.append(Creat1);
 	};
-}
+};
