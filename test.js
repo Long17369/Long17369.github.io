282f4718e8c8aea8637d1a38237ebaad39781444
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-26 05:32:27 +0000

    modified:   index.html 	modified:   test.js 	new file:   word/7.26.json 	deleted:    word/7.26.txt

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
-function fun_1(){
-    document.getElementById('word').innerHTML=word;
+function loadWord(){
+	var request = new XMLHttpRequest();
+	request.open('GET','./word/7.26.json');
+	request.responseType='json';
+	request.send();
+	request.onload = function(){
+		var word = request.response;
+		showWord(word);
+	}
 }
-function inputWord(){
-    
+function showWord(word){
+	var Word = document.getElementById('word');
+	var strWord
+	strWord = stringWord(word)
+	Word.innerHTML=strWord;
+}
+function stringWord(word){
+	var Word = word
 }
