090cf29005e3b3183cc3c84f62c6fd5b33b6f92f
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-26 07:07:11 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function showWord(word){
 	var Word = document.getElementById('word');
 	var Creat;
 	var words = word.word;
+	Word.innerHTML=''
 	for(var i in words){
 		Creat = document.createElement('word'+i);
-		Creat.append(words[i]);
+		Creat.append(words[i]+'\n');
 		Word.append(Creat)
 	};
 }
