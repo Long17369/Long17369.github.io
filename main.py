618ce464b96dc520371172e6760bd5003f67a0fa
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-27 03:50:19 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """翻译"""
+import json #读取json
 from translation.main import translate #翻译
 
 
-translate.complex(input('请输入翻译内容\n'))
+def tran(file):
+    """批量翻译"""
+    with open(file,'r',encoding='utf-8') as f:
+        words = json.load(f)
+        for i in words['words']:
+            translate.complex(i)
+
+if __name__ == '__main__':
+    day = input('请输入日期')
+    tran(day)
