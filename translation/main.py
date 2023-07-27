cab10b1ec26ee15f4a348527543dba5ca8956b58
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-27 03:14:12 +0000

    modified:   translation/main.py 	modified:   translation/setting.json

--- translation/main.py
+++ translation/main.py
@@ -16,68 +16,73 @@ class Translate():
         def __init__(self) -> None:
             self.setting = setting.get('key')
             self.formdata = setting.get('formdata')
-            self.error = setting.get('sign')["translate"]
+            self.error = setting.get('sign')["translate_s"]
             self.url = setting.get('urlsimple')
             self.header = setting.get('header')
             self.formdata.update(setting.get('key'))
         def reload(self,fun):
             """重载key&token"""
-            self.formdata.updata(setting.get('key'))
+            self.formdata.update(setting.get('key'))
             return fun
         def simple(self,info):
             """简单翻译"""
             self.formdata["text"] = info
             post = requests.post(self.url, data=self.formdata, headers=self.header, timeout=10)
             word = post.json()
-            if word == self.error:
-                # print('key, token已失效')
+            if str(word) == str(self.error):
+                print('key, token已失效')
                 keyToken()
-                self.reload(self.simple)(info)
+                word = self.reload(self.simple)(info)
+                input()
             return self.end(word)
         def end(self,word):
             """结束"""
-            text = word[0]["translations"]["text"]
+            text = word[0]["translations"][0]["text"]
             return text
 
     class Complex():
         """复杂翻译"""
         def __init__(self) -> None:
             self.setting = setting.get('key')
-            self.formdata = setting.get('formdata')
-            self.error = setting.get('sign')["translate"]
+            self.formdata = setting.get('formdata_1')
+            self.error = setting.get('sign')["translate_c"]
             self.url = setting.get('urlcomplex')
             self.header = setting.get('header')
             self.formdata.update(setting.get('key'))
         def reload(self,fun):
             """重载key&token"""
-            self.formdata.updata(setting.get('key'))
+            self.formdata.update(setting.get('key'))
             return fun
         def complex(self,info):
             """复杂翻译"""
             self.formdata["text"] = info
             post = requests.post(self.url, data=self.formdata, headers=self.header, timeout=10)
             word = post.json()
-            if word == self.error:
-                # print('key, token已失效')
+            if str(word) == str(self.error):
+                print('key, token已失效')
                 keyToken()
-                self.reload(self.complex)(info)
+                word = self.reload(self.complex)(info)
             return self.end(word,info)
         def end(self,word,info):
             """结束"""
-            text = word[0]["translation"]
+            text = word[0]["translations"]
             dict_complex = {}
-            for i in range(text):
+            for i in text:
                 dict_complex["Chinese"] = i["displayTarget"]
                 english = []
-                for j in i["backTranslations"]:
+                for j in range(len(i["backTranslations"])):
                     english.append(i["backTranslations"][j]["displayText"])
                 dict_complex["English"] = english
-            file = './word/word'+str(info)
+            file = './word/word/'+str(info)+'.json'
             with open(file,'w',encoding='utf-8') as f:
-                json.dump(dict_complex,f,sort_keys=True,indent=True)
+                json.dump(dict_complex,f,sort_keys=True,indent=True,ensure_ascii=False)
 
-
-translate = Translate()
+    def fun_1(self):
+        """凑数用的"""
+        return None
+    def fun_2(self):
+        """凑数用的"""
+        return None
 
 
 def get_key_and_token():
@@ -85,19 +90,17 @@ def get_key_and_token():
     url = 'https://cn.bing.com/translator'
     sign = ('var', 'params_AbusePreventionHelper', '=',)
     html1 = requests.get(url, timeout=10).text
-    print(type(html1))
     text1 = html1.split(';')
     length = len(text1)
     for i in range(length):
         text2 = text1[i].split()
         if len(text2) == 4:
-            Yesnot = [text2[i] == sign[i] for i in range(2)]
+            Yesnot = [text2[i] == sign[i] for i in range(3)]
             yesnot = Yesnot[0] and Yesnot[1] and Yesnot[2]
             if yesnot:
                 text3 = text2[3][1:-1].split(',')
-                print(text3)
                 break
-    key = {"key":text3[0],"token":text3[1]}
+    key = {"key":text3[0],"token":text3[1][1:-1]}
     setting.set("key",key)
 keyToken = get_key_and_token
 
@@ -123,14 +126,14 @@ class Setting():
         settings = self.getSetting()
         settings[settingB] = settingC
         with open('./setting.json', 'w', encoding='utf-8') as f:
-            json.dump(settings,f,sort_keys=True,indent=True)
+            json.dump(settings,f,sort_keys=True,indent=True,ensure_ascii=False)
 
 setting = Setting()
+translate = Translate()
 
 
 if __name__ == '__main__':
     print('          翻译结果由微软翻译提供！（请确保网络已连接）')
-    while True:
-        trans = input('翻译内容：')
-        words = translate.simple(trans)
-        print(words, '\n')
+    trans = input('翻译内容：')
+    words = translate.complex(trans)
+    print(words, '\n')
