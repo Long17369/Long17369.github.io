afb450ac029ba305a7c0fac7289ae4afccab732d
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-27 00:58:01 +0000

    modified:   translation/main.py 	modified:   translation/setting.json

--- translation/main.py
+++ translation/main.py
@@ -5,27 +5,66 @@ import json         # 用于读取json
 import requests     # 用于请求网络
 
 
-def translate(info):
+class Translate():
     """翻译"""
-    token = 'NTuUIMBqLrZQ98bEVlO33MSmXU6QiN5R'
-    key = '1690370670467'
-    sign = "{'statusCode': 205, 'errorMessage': ''}"
-    url1 = getSetting('url1')
-    url2 = '5203357EA2EF4807ADCB196ED95F24D5'
-    url3 = getSetting('url3')
-    url = url1+url2+url3
-    formdata = {
-        "fromLang": "en",
-        "text": info,
-        "to": "zh-Hans",
-        "token": token,
-        "key": key,
-        "tryFetchingGenderDebiasedTranslations": "ture"}
-    header = getSetting('header')
-    word = requests.post(url, data=formdata, headers=header, timeout=10).json()
-    if word == sign:
-        print('key, token已失效')
-    return word
+    def __init__(self) -> None:
+        self.token = 'NTuUIMBqLrZQ98bEVlO33MSmXU6QiN5R'
+        self.key = '1690370670467'
+
+    class Simple():
+        """简单翻译"""
+        def __init__(self) -> None:
+            self.setting = setting.get('key')
+            self.key,self.token = self.setting[0],self.setting[1]
+            self.sign = "{'statusCode': 205, 'errorMessage': ''}"
+            self.url1 = setting.get('url1')
+            self.url2 = '5203357EA2EF4807ADCB196ED95F24D5'
+            self.url3 = setting.get('url3')
+            self.url = self.url1+self.url2+self.url3
+            self.header = setting.get('header')
+        def reload(self):
+            """重载key&token"""
+            self.key,self.token = self.setting[0],self.setting[1]
+        def simple(self,info):
+            """简单翻译"""
+            formdata = {
+                "fromLang": "en",
+                "text": info,
+                "to": "zh-Hans",
+                "token": self.token,
+                "key": self.key,
+                "tryFetchingGenderDebiasedTranslations": "ture"}
+            word = requests.post(self.url, data=formdata, headers=self.header, timeout=10).json()
+            if word == self.sign:
+                print('key, token已失效')
+
+            return word
+
+    class Complex():
+        """复杂翻译"""
+        def __init__(self) -> None:
+            self.setting = setting.get('key')
+            self.formdata = setting.get('formdata')
+            self.key,self.token = self.setting[0],self.setting[1]
+            self.error = setting.get('url1')["translate"]
+            self.url1 = setting.get('url1')
+            self.url2 = '5203357EA2EF4807ADCB196ED95F24D5'
+            self.url3 = setting.get('url3')
+            self.url = self.url1+self.url2+self.url3
+            self.header = setting.get('header')
+        def reload(self):
+            """重载key&token"""
+            self.key,self.token = self.setting[0],self.setting[1]
+        def complex(self,info):
+            """复杂翻译"""
+            self.formdata["text"] = info
+            word = requests.post(self.url, data=self.formdata, headers=self.header, timeout=10).json()
+            if word == self.error:
+                print('key, token已失效')
+                
+            return word
+
+translate = Translate()
 
 
 def get_key_and_token():
@@ -47,24 +86,40 @@ def get_key_and_token():
                 break
     key = text3[0]
     token = text3[1]
+    setting.set()
     return key, token
+keyToken = get_key_and_token
 
+class Setting():
+    """用于读写设置"""
+    def __init__(self) -> None:
+        self.set = self.setSetting
+        self.get = self.getSetting
+    def getSetting(self,settingB: str = 'setting'):
+        """用于读取设置"""
+        with open('./setting.json', 'r', encoding='utf-8') as f:
+            settings = json.load(f)
+        if settingB in settings:
+            return settings[settingB]
+        if settingB == 'setting':
+            return settings
+        print('Setting Not Found')
+        return None
+    def setSetting(self,settingB = None,settingC:str = None):
+        """用于写入设置"""
+        if settingB is None or settingC is None:
+            return None
+        settings = self.getSetting()
+        settings[settingB] = settingC
+        with open('./setting.json', 'w', encoding='utf-8') as f:
+            json.dump(settings,f,sort_keys=True,indent=True)
 
-def getSetting(setting: str = 'setting') -> dict:
-    """用于获取设置"""
-    with open('./setting.json', 'r', encoding='utf-8') as f:
-        settings = json.load(f)
-    if setting in settings:
-        return settings[setting]
-    if setting == 'setting':
-        return settings
-    print('Setting Not Found')
-    return None
+setting = Setting()
 
 
 if __name__ == '__main__':
     print('          翻译结果由微软翻译提供！（请确保网络已连接）')
     while True:
         trans = input('翻译内容：')
-        words = translate(trans)
+        words = translate.simple(trans)
         print(words, '\n')
