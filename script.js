63d27db3f6d2f92f4d609e057a5873b76c752357
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-29 04:40:18 +0000

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Version : 1.0.1.01
-// Date : 2023/07/29 12:05
+// Version : 1.0.1.02
+// Date : 2023/07/29 12:40
 // Author : Long17369
 function showword(who = 'main') {
 	action2.style.display = 'none'
@@ -22,14 +22,22 @@ function Next(info,who){
 	if (info == 'next'){
 		count += 1
 	}
-	next(count,who)
+	next(count,who,info)
 }
-function next(count,who){
+function next(count,who,info){
 	if (word = undefined){
 		word = loadWord(who)
 	}
+	if (info == 'reset'){
+		var SetCount = document.getElementById('Count')
+		SetCount.innerText(length(word.word) + 1)
+	}
+	else{
+		var setcount = document.getElementById('count')
+		setcount.innerText(count + 1)
+	}
 	var SetWord = document.getElementById('word')
-	SetWord.innerText()
+	SetWord.innerText(word.word[count])
 }
 function showWord(word, count) {
 
