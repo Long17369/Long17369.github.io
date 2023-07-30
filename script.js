2351486efff4f5818377bc1906818df4a2fb5431
Long17369 <134676518+Long17369@users.noreply.github.com>
2023-07-30 04:56:48 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,81 +1,84 @@
 // Version : 1.0.1.05
 // Date : 2023/07/29 20:35
 // Author : Long17369
-var word
-var wordL = true
+var word;
+var wordL = true;
 function showword(who) {
-	action2.style.display = 'none'
+	action2.style.display = 'none';
 	wordCard.style.display = '';
-	console.log('载入单词'+who);
+	console.log('载入单词' + who);
 	sleep(who);
-	return show(who, word);
-}
+	setTimeout(() => {
+		show(who);
+	}, 1000);
+};
 function show(who) {
 	var Word = document.getElementById('Word');
 	var Creat;
-	sleep(who)
+	sleep(who);
 	var words = word.word;
 	Word.innerHTML = '';
-}
-function Next(info,who){
-	var count
-	if (info == "reset"){
-		count = 0
+};
+function Next(info, who) {
+	var count;
+	if (info == "reset") {
+		count = 0;
 	}
-	if (info == 'next'){
-		count += 1
-	}
-	next(count,who,info)
-}
-function next(count,who,info){
-	sleep(who)
-	if (info == 'reset'){
-		var SetCount = document.getElementById('Count')
-		SetCount.innerText = (length(word.word) + 1)
-	}
-	else{
-		var setcount = document.getElementById('count')
-		setcount.innerText = (count + 1)
-	}
-	var SetWord = document.getElementById('word')
-	SetWord.innerText = word.word[count]
-}
-function showWord(count) {
-
-}
-function sleep(who){
-	if (wordL){
-		loadWord(who)
-	}
-}
-function loadWord(who) {
-	if (who == 'main') {
-		var date = getDate()
-		var open = './每日单词/' + date + '.json'
-		console.log('main载入中')
+	else if (info == 'next') {
+		count += 1;
+	};
+	next(count, who, info);
+};
+function next(count, who, info) {
+	sleep(who);
+	if (info == 'reset') {
+		var SetCount = document.getElementById('Count');
+		SetCount.innerText = (length(word.word) + 1);
 	}
 	else {
-		if (who == 'history') {
-			return history()
-		}
-		else {
-			if (who == 'all') {
-				return all()
-			}
-			else {
-				console.log('载入失败')
-				return
-			}
-		}
+		var setcount = document.getElementById('count');
+		setcount.innerText = (count + 1);
+	};
+	var SetWord = document.getElementById('word');
+	SetWord.innerText = word.word[count];
+};
+function showWord(count) {
+
+};
+function sleep(who) {
+	if (wordL) {
+		loadWord(who);
+	};
+};
+function loadWord(who) {
+	if (who == 'main') {
+		var date = getDate();
+		var open = './每日单词/' + date + '.json';
+		console.log('main载入中');
 	}
-	wordL = false
+	else if (who == 'history') {
+		return history();
+	}
+	else if (who == 'all') {
+		return all();
+	}
+	else {
+		console.log('载入失败');
+		return
+	};
+	wordL = false;
 	var request = new XMLHttpRequest();
 	request.open('GET', open);
 	request.responseType = 'json';
 	request.send();
 	request.onload = function () {
 		word = request.response;
-		console.log('main载入成功')
+		if (word == null) {
+			console.log('main载入失败');
+		}
+		else {
+			console.log('main载入成功');
+		};
 	};
 };
 // function showWord(word) {
@@ -92,15 +95,15 @@ function loadWord(who) {
 // 	};
 // };
 function getDate() {
-	var date = new Date()
-	var strDate = date.getDate()
-	var nowMonth = date.getMonth() + 1
-	var nowDate = nowMonth + '.' + strDate
-	return nowDate
-}
+	var date = new Date();
+	var strDate = date.getDate();
+	var nowMonth = date.getMonth() + 1;
+	var nowDate = nowMonth + '.' + strDate;
+	return nowDate;
+};
 function showChinese() {
 
-}
+};
 // function showword() {
 // 	showWord(loadWord())
 // }
