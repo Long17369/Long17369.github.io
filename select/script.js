92b0495112b7d52dbddb64be5cc4c62bbfa06eed
Long17369 <long17369@foxmail.com>
2023-10-21 21:26:49 +0800

    modified:   select/script.js

--- select/script.js
+++ select/script.js
@@ -11,40 +11,57 @@ var word = {
 		'不认识': [],
 		'finish': true
 	},
-	'Words':[]
+	'Words': [],
+	'randomNumber': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+	'next': [false],
+	'Next': false
 };
 $.ajax({
-	type:"get", //使用get方式
+	type: "get", //使用get方式
 	url: "../word/word.json", //json文件相对于这个HTML的路径
-	dataType:"json",
-	success:function(data) {
+	dataType: "json",
+	success: function (data) {
 		word.Words = data;
-		for (const i in word.Words){
+		for (const i in word.Words) {
 			for (const j in word.Words[i].word) {
 				var exist = false;
 				for (const k in word.word) {
-					if (word.word[k] == word.Words[i].word[j]){
+					if (word.word[k] == word.Words[i].word[j]) {
 						exist = true;
 						// console.log(word.word[k]);
 						// console.log(word.Words[i].word[j]);
 					};
 				};
-				if (exist){}
-				else{
+				if (exist) { }
+				else {
 					word.word.push(word.Words[i].word[j])
 				};
 			};
 		};
 		//这个data就是json数据
 	},
-	error:function() {
+	error: function () {
 		alert("请求失败");
 	}
 });
 function select() {
-	var randomNumber
 	for (let i = 0; i < 10; i++) {
-		randomNumber = Math.floor(Math.random() * word.word.length);
-		document.getElementById('word'+i).innerHTML = word.word[randomNumber];
+		while (!word.Next) {
+			word.randomNumber[i] = Math.floor(Math.random() * word.word.length);
+			console.log(word.randomNumber)
+			for (const j in word.randomNumber) {
+				if (i !== j) {
+					word.next[j] = false;
+				}
+				else {
+					if (word.randomNumber[j] == word.randomNumber[i]) {
+						word.next[j] = true;
+					}
+				}
+			}
+			word.Next = !(word.next.some(ele => ele == true))
+		};
+		document.getElementById('word' + i).innerHTML = word.word[word.randomNumber[i]];
+		word.Next = false
 	}
 }
