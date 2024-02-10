1ea36d36b7ce75a37cdb4f33e8a146bf9136df01
Long17369 <long17369@foxmail.com>
2024-02-10 19:19:09 +0800

    开始优化

---
+++ 每日单词/dir.py
@@ -0,0 +1,15 @@
+'''每日单词目录
+Version : 1.0.0.0
+Date : 2024/02/10 17:45
+Author : Long17369
+'''
+
+import json         # 用于读取json
+
+class Dirtory():
+    def __init__(self) -> None:
+        pass
+    
+    def getDirtory():
+        
+        ...
